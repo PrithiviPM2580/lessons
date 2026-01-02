290ec1c8085f5c3269bd256dfc87d9b18de11d51
Prithivi <prithivi@users.noreply.github.com>
2026-01-02 12:31:53 +0545

    Add app3.js and update configuration for multiple instances in nginx setup

---
+++ nginx-load-balancer/src/app3.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send(`Welcome to Instance ${process.env.INSTANCE_ID2}`);
+});
+
+const PORT3 = process.env.PORT3 || 3002;
+app.listen(PORT3, () => {
+  console.log(`Server is running on port ${PORT3}`);
+});
