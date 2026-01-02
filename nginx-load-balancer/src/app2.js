5ff36a7d55c11541135830d4393862115164e3ea
Prithivi <prithivi@users.noreply.github.com>
2026-01-02 11:52:19 +0545

    Refactor environment configuration and add initial Express application files

---
+++ nginx-load-balancer/src/app2.js
@@ -0,0 +1,12 @@
+import express from "express";
+import route from "./routes/index.js";
+
+const app = express();
+
+app.use(express.json());
+app.use("/", route);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
