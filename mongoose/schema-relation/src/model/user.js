f1b7e36036b3266c19ba600e250d8581681d7049
Prithivi <prithivi@users.noreply.github.com>
2026-02-02 14:01:11 +0545

    Enhance user model and queries with additional features and optimizations

--- mongoose/schema-relation/src/model/user.js
+++ mongoose/schema-relation/src/model/user.js
@@ -40,14 +40,53 @@ const userSchema = new mongoose.Schema(
   },
   {
     timestamps: true,
-    toJSON: { virtuals: true },
     toObject: { virtuals: true },
+    toJSON: { virtuals: true },
   },
 );
+
+userSchema.pre(
+  "deleteOne",
+  { document: true, query: false },
+  async function (next) {
+    await this.model("Post").deleteMany({ author: this._id });
+    next();
+  },
+);
+
+userSchema.statics.findByUserId = function (id) {
+  return this.findById(id);
+};
+
+userSchema.query.active = function () {
+  return this.where({ isActive: true });
+};
 
 userSchema.virtual("fullName").get(function () {
   return `${this.firstName} ${this.lastName}`;
 });
+
+userSchema.virtual("posts", {
+  ref: "Post",
+  localField: "_id",
+  foreignField: "author",
+  justOne: true,
+});
+
+// 1. Unique email
+userSchema.index({ email: 1 }, { unique: true });
+
+// 2. Compound index for dashboard queries
+userSchema.index({ role: 1, isActive: 1 });
+
+// 3. Text index for search
+userSchema.index({ firstName: "text", lastName: "text" });
+
+// 4. Partial index (optional)
+userSchema.index(
+  { email: 1 },
+  { unique: true, partialFilterExpression: { isActive: true } },
+);
 
 const User = mongoose.model("User", userSchema);
 export default User;
--- mongoose/schema-relation/src/queries.js
+++ mongoose/schema-relation/src/queries.js
@@ -9,12 +9,52 @@ connectDB();
 
 async function getAllUsers() {
   try {
-    const users = await User.find(); // Use await for async
-    console.log(users);
+    const users = await User.find().populate("posts"); // Use await for async
+    // console.log(users);
+    console.log(JSON.stringify(users, null, 2));
     return users;
   } catch (err) {
     console.error(err);
   }
 }
 
-getAllUsers();
+// getAllUsers();
+
+async function getOneUser(userId) {
+  try {
+    const user = await User.findByUserId(userId);
+    console.log(user.fullName);
+    console.log(user);
+    return user.fullName;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// getOneUser("698038027c3cb33e193835c0");
+
+async function getActiveUsers() {
+  try {
+    const users = await User.find().active();
+    console.log(users);
+    return users;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// getActiveUsers();
+
+async function deleteUserById(userId) {
+  try {
+    const user = await User.findById(userId);
+    if (!user) return console.log("User not found");
+
+    await user.deleteOne();
+    console.log(`User with ID ${userId} deleted successfully`);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+deleteUserById("6980527a9e36def50a312829");
