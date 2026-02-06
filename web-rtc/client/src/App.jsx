fa53cc44ad9f0760161661ab0086339747ebb955
Prithivi <prithivi@users.noreply.github.com>
2026-02-06 14:31:05 +0545

    feat: create Home component and update routing to use it

--- web-rtc/client/src/App.jsx
+++ web-rtc/client/src/App.jsx
@@ -1,9 +1,10 @@
 import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<h1>Home Page</h1>} />
+      <Route path="/" element={<Home />} />
     </Routes>
   );
 };
