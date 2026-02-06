b04c324afceb6e6f865633d8da4645825dba9491
Prithivi <prithivi@users.noreply.github.com>
2026-02-06 17:42:34 +0545

    feat: enhance Home component with room joining functionality and input handling

--- web-rtc/client/src/pages/Home.jsx
+++ web-rtc/client/src/pages/Home.jsx
@@ -1,12 +1,42 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { useSocket } from "../providers/Socket";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const { socket } = useSocket();
+  const navigate = useNavigate();
+
+  const [email, setEmail] = useState("");
+  const [roomId, setRoomId] = useState("");
+
+  const handleRoomJoined = (data) => {
+    navigate(`/room/${data.roomId}`);
+  };
+
+  useEffect(() => {
+    socket.on("joined-room", handleRoomJoined);
+  }, [socket]);
+
+  const handleJoinRoom = () => {
+    socket.emit("join-room", { emailId: email, roomId });
+  };
+
   return (
     <div className="homepage-container">
       <div className="input-container">
-        <input type="email" placeholder="Enter your email here..." />
-        <input type="text" placeholder="Enter Room Code..." />
-        <button>Enter Room</button>
+        <input
+          type="email"
+          placeholder="Enter your email here..."
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Enter Room Code..."
+          value={roomId}
+          onChange={(e) => setRoomId(e.target.value)}
+        />
+        <button onClick={handleJoinRoom}>Enter Room</button>
       </div>
     </div>
   );
