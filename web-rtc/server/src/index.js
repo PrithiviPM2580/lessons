d6f7773562a5fd44a092e57de72eac1049e49e32
Prithivi <prithivi@users.noreply.github.com>
2026-02-06 18:29:52 +0545

    feat: add socketIdToEmailMap for managing user call connections

--- web-rtc/server/src/index.js
+++ web-rtc/server/src/index.js
@@ -10,6 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const emailToSocketMap = new Map();
+const socketIdToEmailMap = new Map();
 
 io.on("connection", (socket) => {
   console.log("A user connected");
@@ -17,9 +18,25 @@ io.on("connection", (socket) => {
     console.log(`User with ${data.emailId} joined room ${data.roomId}`);
     const { roomId, emailId } = data;
     emailToSocketMap.set(emailId, socket.id);
+    socketIdToEmailMap.set(socket.id, emailId);
     socket.join(roomId);
     socket.emit("joined-room", { roomId });
     socket.broadcast.to(roomId).emit("user-joined", { emailId });
+  });
+
+  socket.on("call-user", (data) => {
+    const { emailId, offer } = data;
+    const fromEmail = socketIdToEmailMap.get(socket.id);
+    const socketId = emailToSocketMap.get(emailId);
+    if (socketId) {
+      io.to(socketId).emit("incoming-call", { from: fromEmail, offer });
+    }
+  });
+
+  socket.on("call-accepted", (data) => {
+    const { emailId, ans } = data;
+    const socketId = emailToSocketMap.get(emailId);
+    socket.to(socketId).emit("call-accepted", { ans });
   });
 });
 
