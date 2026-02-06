43a2ae9f9badc0736b3101aa4d03697a660cf048
Prithivi <prithivi@users.noreply.github.com>
2026-02-06 18:29:34 +0545

    feat: restructure App component to include PeerProvider and update Room route

--- web-rtc/client/src/App.jsx
+++ web-rtc/client/src/App.jsx
@@ -1,15 +1,19 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import { SocketProvider } from "./providers/Socket";
+import { PeerProvider } from "./providers/Peer";
+import Room from "./pages/Room";
 
 const App = () => {
   return (
-    <SocketProvider>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/room/:roomId" element={<div>Room Page</div>} />
-      </Routes>
-    </SocketProvider>
+    <PeerProvider>
+      <SocketProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/room/:roomId" element={<Room />} />
+        </Routes>
+      </SocketProvider>
+    </PeerProvider>
   );
 };
 
